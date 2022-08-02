7b6a67e6699ef4588b69e9da54a7225275c9dcde
d-takeuchi <kuronk.kuro3@gmail.com>
2022-08-02 21:24:46 +0900

    add: github-pagesデプロイ設定

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  basePath: process.env.GITHUB_ACTIONS && '/Nextjs-Emotion',
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
