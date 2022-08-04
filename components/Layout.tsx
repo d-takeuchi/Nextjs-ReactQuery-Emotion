0297de3002ba0815f21c6dc42c47f8d49f5469fa
d-takeuchi <kuronk.kuro3@gmail.com>
2022-08-04 21:55:37 +0900

    style: レイアウト修正

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -8,25 +8,53 @@ interface Props {
   title: string
 }
 
-const hello = css`
-  color: pink;
+const header = css`
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 0 4%;
+  display: flex;
+  justify-content: space-between;
+`
+const mainNav = css`
+  display: flex;
+  font-size: 1.25rem;
+  margin-top: 34px;
+  list-style: none;
+  li {
+    margin-left: 36px;
+  }
+  a {
+    color: #432;
+  }
+  a:hover {
+    color: pink;
+  }
+`
+const wrapper = css`
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 0 4%;
 `
 const Layout: FC<Props> = ({ children, title }) => {
   return (
-    <div css={hello}>
+    <div>
       <Head>
         <title>{title}</title>
       </Head>
-      <header>
+      <header css={[header, wrapper]}>
+        <h1>EmotionとReactQuery(GraphQL)を試す</h1>
         <nav>
-          <ul>
+          <ul css={mainNav}>
             <li>
-              <Link href="/doragons">ドラゴン一覧</Link>
+              <Link href="/dragons">React Query Basic GraphQL</Link>
+            </li>
+            <li>
+              <Link href="/dragons">Mutation</Link>
             </li>
           </ul>
         </nav>
       </header>
-      <main>{children}</main>
+      <main css={wrapper}>{children}</main>
     </div>
   )
 }
