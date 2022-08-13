6d9bbf7c06fd3061a5a39f680880f18f6b5a108e
d-takeuchi <kuronk.kuro3@gmail.com>
2022-08-14 01:57:15 +0900

    wip

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,27 @@ import type { AppProps } from 'next/app'
 import { useState } from 'react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { Provider } from 'react-redux'
+import { store } from '../app/store'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [queryClient] = useState(() => new QueryClient())
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            retry: false,
+            refetchOnWindowFocus: false,
+          },
+        },
+      })
+  )
 
   return (
     <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
       <ReactQueryDevtools />
     </QueryClientProvider>
   )
--- queries/queries.ts
+++ queries/queries.ts
@@ -1,9 +1,38 @@
 import { gql } from 'graphql-request'
 
-//ドラゴン一覧を取得
-export const GET_DRAGONS = gql`
-  query GetDragons {
-    dragons {
+export const GET_DRAGON = gql`
+  query GetDragon {
+    dragon {
+      id
+      name
+      description
+    }
+  }
+`
+export const CREATE_DRAGON = gql`
+  mutation CreateDragon($name: String!, $description: String!) {
+    insert_dragon_one(object: { name: $name, description: $decription }) {
+      id
+      name
+      description
+    }
+  }
+`
+export const UPDATE_DRAGON = gql`
+  mutation UpdateDragon($id: uuid!, $name: String!, $description: String!) {
+    update_dragon_by_pk(
+      pk_colmuns: { id: $id }
+      _set: { name: $name, description: $description }
+    ) {
+      id
+      name
+      description
+    }
+  }
+`
+export const DELETE_DRAGON = gql`
+  mutation DeleteDragon($id: uuid!) {
+    delete_dragon_by_pk(pk_colmuns: { id: $id }) {
       id
       name
       description
