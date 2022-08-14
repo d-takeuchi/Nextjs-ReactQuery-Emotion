b3d110cfaa5e9055d7c6385d62a407e4c774ddab
d-takeuchi <kuronk.kuro3@gmail.com>
2022-08-14 12:57:18 +0900

    wip

--- pages/dragons.tsx
+++ pages/dragons.tsx
@@ -3,6 +3,7 @@ import { FC } from 'react'
 import { DragonListMemo } from '../components/DragonList'
 import Layout from '../components/Layout'
 import { css } from '@emotion/react'
+import { DragonEditMemo } from '../components/DragonEdit'
 
 const dragonName = css`
   font-weight: bold;
@@ -13,6 +14,7 @@ const Dragons: FC = () => {
     <Layout title="ドラゴン一覧">
       <h1>ドラゴン一覧</h1>
 
+      <DragonEditMemo />
       <DragonListMemo />
       <Link href="/">ホームへ戻る</Link>
     </Layout>
